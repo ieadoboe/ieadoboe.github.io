254ce8c8628f6e98db0989061919f05e06a9fbca
Isaac Adoboe <ikeadoboe1@gmail.com>
2024-12-18 02:06:56 -0330

    Update github pages portfolio basepath

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   output: "export",
 
   // Required for GitHub Pages deployment
-  basePath: "/ieadoboe.github.io", // Replace with your repository name
-  assetPrefix: "/ieadoboe.github.io",
+  // basePath: "/ieadoboe.github.io", // Replace with your repository name
+  // assetPrefix: "/ieadoboe.github.io",
 
   // Optional: Allows images without optimization (needed for static export)
   images: {
