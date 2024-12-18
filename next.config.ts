faac7b4a69579d3ac99e7ee3c03c3b15c49ebbc4
Isaac Adoboe <ikeadoboe1@gmail.com>
2024-12-18 02:17:19 -0330

    Remove base path

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   // Enables static HTML export
   output: "export",
 
-  // Required for GitHub Pages deployment
-  // basePath: "/ieadoboe.github.io", // Replace with your repository name
-  // assetPrefix: "/ieadoboe.github.io",
-
   // Optional: Allows images without optimization (needed for static export)
   images: {
     unoptimized: true,
