8edf051f8b7df1db7c1240a9be2378eed4e05e39
Isaac Adoboe <ikeadoboe1@gmail.com>
2025-01-02 21:26:04 -0330

    updates on latestpostitem

--- app/components/LatestPostItem.tsx
+++ app/components/LatestPostItem.tsx
@@ -18,46 +18,46 @@ const HomePostItem = ({
   const formattedDate = formatDate(date);
 
   return (
-    <article className="group relative flex flex-col items-start">
-      {/* Hover overlay */}
-      <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
+    <Link href={slug} passHref>
+      <article className="group relative flex flex-col items-start cursor-pointer">
+        {/* Hover overlay */}
+        <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
 
-      {/* Date */}
-      <time
-        className="relative z-10 mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
-        dateTime={date}
-      >
-        <span
-          className="absolute inset-y-0 left-0 flex items-center"
-          aria-hidden="true"
+        {/* Date */}
+        <time
+          className="relative z-10 mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
+          dateTime={date}
         >
-          <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
-        </span>
-        {formattedDate}
-      </time>
+          <span
+            className="absolute inset-y-0 left-0 flex items-center"
+            aria-hidden="true"
+          >
+            <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
+          </span>
+          {formattedDate}
+        </time>
 
-      {/* Title */}
-      <h2 className="relative z-10 text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-        <Link href={slug}>
+        {/* Title */}
+        <h2 className="relative z-10 text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
           <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
           <span className="relative z-10">{title}</span>
-        </Link>
-      </h2>
+        </h2>
 
-      {/* Description */}
-      <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-        {description}
-      </p>
+        {/* Description */}
+        <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+          {description}
+        </p>
 
-      {/* Read Article */}
-      <div
-        aria-hidden="true"
-        className="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
-      >
-        Read article
-        <Icons.arrowright />
-      </div>
-    </article>
+        {/* Read Article */}
+        <div
+          aria-hidden="true"
+          className="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
+        >
+          Read article
+          <Icons.arrowright />
+        </div>
+      </article>
+    </Link>
   );
 };
 
