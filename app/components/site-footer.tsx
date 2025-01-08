3be33e2716e8a5a96dd06739fc38943fd0e5597d
Isaac Adoboe <ikeadoboe1@gmail.com>
2025-01-08 01:01:05 -0330

    footer update

--- app/components/site-footer.tsx
+++ app/components/site-footer.tsx
@@ -10,11 +10,33 @@ const Footer = () => {
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <Link
+                  key="home"
+                  href="/"
+                  className="transition hover:text-teal-500 dark:hover:text-teal-400"
+                >
+                  Home
+                </Link>
+                <Link
                   key="about"
                   href="/about"
                   className="transition hover:text-teal-500 dark:hover:text-teal-400"
                 >
                   About
+                </Link>
+                <Link
+                  key="blog"
+                  href="/blog"
+                  className="transition hover:text-teal-500 dark:hover:text-teal-400"
+                >
+                  Articles
+                </Link>
+
+                <Link
+                  key="products"
+                  href="/products"
+                  className="transition hover:text-teal-500 dark:hover:text-teal-400"
+                >
+                  Products
                 </Link>
                 <Link
                   key="projects"
@@ -24,18 +46,11 @@ const Footer = () => {
                   Projects
                 </Link>
                 <Link
-                  key="blog"
-                  href="/blog"
+                  key="spotify"
+                  href="/spotify"
                   className="transition hover:text-teal-500 dark:hover:text-teal-400"
                 >
-                  Blog
-                </Link>
-                <Link
-                  key="products"
-                  href="/products"
-                  className="transition hover:text-teal-500 dark:hover:text-teal-400"
-                >
-                  Products
+                  Spotify
                 </Link>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
