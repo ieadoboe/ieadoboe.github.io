007ba9cb025544ee3aa884e631566a62d8a4e3bf
Isaac Adoboe <ikeadoboe1@gmail.com>
2025-01-13 21:26:09 -0330

    refactor: simplify Next.js configuration by removing unused MDX setup and plugins

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,54 @@
+// import { build } from "velite";
+// import remarkGfm from 'remark-gfm'
+// import createMDX from '@next/mdx'
+
+// class VeliteWebpackPlugin {
+//   static started = false;
+//   apply(/** @type {import('webpack').Compiler} */ compiler) {
+//     // executed three times in nextjs
+//     // twice for the server (nodejs / edge runtime) and once for the client
+//     compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
+//       if (VeliteWebpackPlugin.started) return;
+//       VeliteWebpackPlugin.started = true;
+//       const dev = compiler.options.mode === "development";
+//       await build({ watch: dev, clean: !dev });
+//     });
+//   }
+// }
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**", // Be cautious: this allows images from all domains
+//       },
+//     ],
+//   },
+//   webpack: (config, { dev }) => {
+//     if (dev) {
+//       // Disable cache for development mode
+//       config.cache = false;
+//     }
+
+//     config.plugins.push(new VeliteWebpackPlugin());
+//     return config;
+//   },
+// };
+
+// const withMDX = createMDX({
+//   options: {
+//     remarkPlugins: [remarkGfm],
+//     rehypePlugins: [],
+//   },
+// })
+
+// export default withMDX(nextConfig)
+
+
 import { build } from "velite";
-import remarkGfm from 'remark-gfm'
-import createMDX from '@next/mdx'
 
 class VeliteWebpackPlugin {
   static started = false;
@@ -18,7 +66,6 @@ class VeliteWebpackPlugin {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
     remotePatterns: [
       {
@@ -38,11 +85,4 @@ const nextConfig = {
   },
 };
 
-const withMDX = createMDX({
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
-  },
-})
-
-export default withMDX(nextConfig)
+export default nextConfig;
