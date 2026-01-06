b20ea14df8eaebe3cd388c87e466682239e01506
ieadoboe <ikeadoboe1@gmail.com>
2026-01-06 04:04:25 -0330

    Add CMS support for Site Settings and Page Content via JSON files

--- config/site.ts
+++ config/site.ts
@@ -1,14 +1,5 @@
-export const siteConfig = {
-  name: "Isaac Adoboe - Computational scientist and Innovator",
-  url: "https://ieadoboe.github.io",
-  description: "Computational scientist, hobbyist chef, and part-time overthinker. 🤔",
-  author: "Isaac Edem Adoboe",
-  links: {
-    twitter: "https://twitter.com/ieadoboe",
-    github: "https://github.com/ieadoboe",
-    vsco: "https://vsco.co/meetbeezy",
-    linkedin: "https://www.linkedin.com/in/isaacedemadoboe",
-  },
-};
+import siteConfigData from "./site.json";
+
+export const siteConfig = siteConfigData;
 
 export type SiteConfig = typeof siteConfig;
